2b447fea7e845b8da2d8822dcfb02d1a6156c878
Tay-Dzonu-QA <tdzonu@qa.com>
2020-10-02 12:20:06 +0100

    created controllers

---
+++ src/main/java/com/qa/TDL_Project/rest/TaskListController.java
@@ -0,0 +1,65 @@
+package com.qa.TDL_Project.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.TDL_Project.dto.TaskListDTO;
+import com.qa.TDL_Project.persistence.domain.TaskList;
+import com.qa.TDL_Project.service.TaskListService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/taskList")
+public class TaskListController {
+	
+	TaskListService service;
+
+	@Autowired
+	public TaskListController(TaskListService service) {
+		super();
+		this.service = service;
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<TaskListDTO> create(@RequestBody TaskList dto){
+//		TaskListDTO created =this.service.create(taskListDTO);
+		return new ResponseEntity<>(this.service.create(dto),HttpStatus.CREATED);
+	}
+	@GetMapping("/read")
+	public ResponseEntity<List<TaskListDTO>> read(){
+		return ResponseEntity.ok(this.service.read());
+	}
+	
+	@GetMapping("/read/{id}")
+	public ResponseEntity<TaskListDTO> read(@PathVariable Long id){
+		return ResponseEntity.ok(this.service.read(id));
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<TaskListDTO> update(@PathVariable Long id,
+			@RequestBody TaskListDTO taskListDTO){
+//		TaskListDTO updated = this.service.update(taskListDTO, id);
+		return new ResponseEntity<>(this.service.update(taskListDTO, id),HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<TaskListDTO> delete(@PathVariable Long id){
+		return this.service.delete(id)
+				? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
+
+
